fc21e9e5f3b41edc9ad67225dd0d3a9bbbb9ec7a
Danny Reyes <reyesdmusic@gmail.com>
2020-04-23 14:32:55 -0400

    fully functioning, just need to update comments, readme, and push to heroku

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -32,9 +32,50 @@ module.exports = function(app) {
     } else {
         console.log("Success! You've saved a note!");
     }
-
+    res.json(finalObject);
 });
   
+});
+
+// app.get("/api/notes/:id", function(req, res) {
+    
+//   var chosen = req.params.id;
+
+//   console.log(chosen);
+
+//   for (var i = 0; i < notesArray.length; i++) {
+//     if (chosen === notesArray[i].id) {
+//       return res.json(notesArray[i]);
+//     }
+//   }
+//   return res.json(false);
+
+// });
+
+app.delete("/api/notes/:id", function(req, res) {
+    
+  var chosen = req.params.id;
+
+  console.log(chosen);
+
+  for (var i = 0; i < notesArray.length; i++) {
+    if (chosen === notesArray[i].id) {
+      notesArray.splice(i, 1);
+      let finalObject = JSON.stringify(notesArray, null, 2);
+      fs.writeFile(outputPath, finalObject, function(err){
+        if (err) {
+          console.log(err);
+      } else {
+        res.json(finalObject);
+          console.log("Success! You've deleted a note!");
+      }
+
+      });
+
+    }
+  }
+  
+
 });
 
 
